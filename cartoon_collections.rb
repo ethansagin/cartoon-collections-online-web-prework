3c69bb8224c3e83b4bd95cf6c7ffb6eb08a9b2ed
Ethan <ethansagin@gmail.com>
2019-03-13 20:59:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,5 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_index = array.index{|x| cheese_types.include?(x) == true}
-  if cheese_index == []
-    nil
-  else 
     array[cheese_index]
-  end
 end
